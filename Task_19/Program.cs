b0f820183f7a49282149b26989af113cab1c9711
Valentina <vkpavlenina@mail.ru>
2022-09-02 23:35:47 +0500

    Task_19 project

---
+++ Task_19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите пятизначное число ");
+        string number = Console.ReadLine();
+        if (number[0] == number[4])
+        {
+            if (number[1] == number[3])
+            {
+                Console.WriteLine("Является палиндромом");
+            }
+        else Console.WriteLine("Не является палиндромом");
+        }
+        else Console.WriteLine("Не является палиндромом");
+    }
+}
+
